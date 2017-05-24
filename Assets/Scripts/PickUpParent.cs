738314e67132392009ecf6b2c2aa7749a53a1ee1
Rahul Kukreja <rkuk591@gmail.com>
2017-05-24 12:36:16 -0700

    PickUpParent script parenting

---
+++ Assets/Scripts/PickUpParent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickUpParent : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Trigger collision with " + other.name);
+
+        if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger touched during collision with " + other.name);
+            other.attachedRigidbody.isKinematic = true;
+            other.gameObject.transform.SetParent(this.transform);
+        }
+
+        if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger up event during collision with " + other.name);
+            other.gameObject.transform.SetParent(null);
+            other.attachedRigidbody.isKinematic = false;
+        }
+    }
+}
